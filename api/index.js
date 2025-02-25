75d482d5e561396915a6aa7d4980aff6de2b1ea9
erhmprah <erhmprah@gmail.com>
2025-02-25 10:50:02 -0800

    changed index for vercel

--- api/index.js
+++ api/index.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const qs = require("qs");
 const path = require("path");
-require("dotenv").config();
-const port = process.env.PORT || 4000;
+
 const app = express();
 
+// Middleware for parsing URL-encoded data
 app.use(
   express.urlencoded({ extended: true, parameterLimit: 100, queryParser: qs })
 );
+
+// Middleware for parsing JSON data
 app.use(express.json());
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
+
+// Serve files from the "uploads" folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-//get routes ...................
+// Routes for users and admin
 app.use("/", require("../routes/users"));
 app.use("/admin", require("../routes/admin"));
 
-app.listen(port, (err) => {
-  if (err) {
-    console.error(`cannot connect to ${port}`);
-  } else {
-    console.log(`listening to port ${port}`);
-  }
-});
+// Serverless handler for Vercel
+module.exports = (req, res) => {
+  app(req, res); // Pass the request and response to Express
+};
